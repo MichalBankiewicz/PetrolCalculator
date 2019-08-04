538939cf4d9bc2ceeed0e1d46e9b5d98442141aa
Michal Bankiewicz <m.bankiewicz01@gmail.com>
2019-08-04 14:12:54 +0200

    po poprawkach

--- src/main/java/pl/akademiakodu/kalkulatorpaliwa/Model/CostCalculator.java
+++ src/main/java/pl/akademiakodu/kalkulatorpaliwa/Model/CostCalculator.java
@@ -1,10 +1,10 @@
 package pl.akademiakodu.kalkulatorpaliwa.Model;
 
 public class CostCalculator {
-    public Double costCalculator(double routeInKilometers, double averageCombustion, double fuelPricePerLiter){
+/*    public Double costCalculator(double routeInKilometers, double averageCombustion, double fuelPricePerLiter){
         double result=((routeInKilometers/100)*averageCombustion)*fuelPricePerLiter;
         return result;
-    }
+    }*/
     public Double costPerPerson(double routeInKilometers, double averageCombustion, double fuelPricePerLiter, int howManyPeople){
         double result2=(((routeInKilometers/100)*averageCombustion)*fuelPricePerLiter)/howManyPeople;
         return result2;
